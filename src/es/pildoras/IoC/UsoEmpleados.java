4a56edb545b3f58dca0adcbbd38700bf97b81801
Diego Fernando Gomez Alvarez <diegofga96@gmail.com>
2020-07-13 14:45:38 -0500

    Serving bean with xml configuration file[video 8]

--- src/es/pildoras/IoC/UsoEmpleados.java
+++ src/es/pildoras/IoC/UsoEmpleados.java
@@ -1,14 +1,21 @@
 package es.pildoras.IoC;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class UsoEmpleados {
 
 	public static void main(String[] args) {
 		
 		//Creacion de objetos de tipo empleado
-		Empleado empleado1=new DirectorEmpleado();
-	
+		//Empleado empleado1=new DirectorEmpleado();
 		//Uso de objetos creados
-		System.out.println(empleado1.getTareas());
+		//System.out.println(empleado1.getTareas());
+		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Empleado Juan = context.getBean("myEmployee", Empleado.class);
+		System.out.println(Juan.getTareas());
+		
+		context.close();
 	}
 
 }
