ad8c176b4775fa65458b2bbba846d4bd41c30394
Diego Fernando Gomez Alvarez <diegofga96@gmail.com>
2020-07-15 16:30:56 -0500

    Ejemplo de autowired en contructor, en seter y en atributo

--- PISC_2/src/es/pildoras/Annotations/ComercialExperimentado.java
+++ PISC_2/src/es/pildoras/Annotations/ComercialExperimentado.java
@@ -6,11 +6,18 @@ import org.springframework.stereotype.Component;
 @Component("ComercialExperimentado")
 public class ComercialExperimentado implements Empleado {
 
-	@Autowired
+	// Autowired con el contructor
+	/*@Autowired
 	public ComercialExperimentado(CreacionInformeFinanciero nuevoInforme) {
 		this.nuevoInforme = nuevoInforme;
-	}
-
+	}*/
+	
+	// Autowired con setter
+	/*@Autowired
+	public void setNuevoInforme(CreacionInformeFinanciero nuevoInforme) {
+		this.nuevoInforme = nuevoInforme;
+	}*/
+	
 	@Override
 	public String getTareas() {
 		return "Vender, vender y vender mas!";
@@ -20,7 +27,8 @@ public class ComercialExperimentado implements Empleado {
 	public String getInofrme() {
 		return nuevoInforme.getInformefinanciero();
 	}
-	
+
+	@Autowired
 	private CreacionInformeFinanciero nuevoInforme;
 
 }
