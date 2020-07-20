db9e0929dd245d56d47d5c09711aef78ffe2f699
Diego Fernando Gomez Alvarez <diegofga96@gmail.com>
2020-07-20 13:13:47 -0500

    MVC Web Spring project

---
+++ PISC_3/src/es/pildoras/spring/mvc/PageController.java
@@ -0,0 +1,13 @@
+package es.pildoras.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+	@RequestMapping
+	String muestraPagina() {
+		return "paginaEjemplo";
+	}
+}
